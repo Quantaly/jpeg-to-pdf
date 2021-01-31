76a52cd1bef6aede90d51e7c16842f2d1b521f97
Kai Page <kai@quantaly.net>
2021-01-31 12:52:33 -0700

    I did a thing

---
+++ src/lib.rs
@@ -0,0 +1,156 @@
+use exif::{Field, In, Reader as ExifReader, Tag, Value};
+use jpeg_decoder::{Decoder as JpegDecoder, PixelFormat};
+use ori::Orientation;
+use printpdf::*;
+use std::fmt::{self, Display, Formatter};
+use std::io::{prelude::*, BufWriter, Cursor};
+
+#[macro_use]
+extern crate lazy_static;
+
+mod ori;
+
+lazy_static! {
+    static ref DEFAULT_ORIENTATION: Field = Field {
+        tag: Tag::Orientation,
+        ifd_num: In::PRIMARY,
+        value: Value::Short(vec![1]),
+    };
+}
+
+pub fn create_pdf_from_jpegs(
+    jpegs: Vec<Vec<u8>>,
+    out: &mut BufWriter<impl Write>,
+    dpi: Option<f64>,
+) -> Result<(), Error> {
+    let dpi = dpi.unwrap_or(300.0);
+
+    let doc = PdfDocument::empty("");
+
+    for (index, image) in jpegs.into_iter().enumerate() {
+        if let Err(cause) = add_page(image, &doc, dpi) {
+            return Err(Error { index, cause });
+        }
+    }
+
+    doc.save(out).map_err(|e| Error {
+        index: 0,
+        cause: Cause::PdfWrite(e),
+    })
+}
+
+fn add_page(image: Vec<u8>, doc: &PdfDocumentReference, dpi: f64) -> Result<(), Cause> {
+    let mut decoder = JpegDecoder::new(Cursor::new(&image));
+    decoder.read_info()?;
+
+    match decoder.info() {
+        None => Err(Cause::UnexpectedImageInfo),
+        Some(info) => {
+            let exif = ExifReader::new().read_from_container(&mut Cursor::new(&image))?;
+
+            let ori = match &exif
+                .get_field(Tag::Orientation, In::PRIMARY)
+                .unwrap_or(&DEFAULT_ORIENTATION)
+                .value
+            {
+                Value::Short(v) => *v.first().unwrap_or(&1),
+                _ => 1,
+            };
+
+            let ori = Orientation {
+                value: ori,
+                width: info.width,
+                height: info.height,
+            };
+
+            let (page, layer) = doc.add_page(
+                Px(ori.display_width() as usize).into_pt(dpi).into(),
+                Px(ori.display_height() as usize).into_pt(dpi).into(),
+                "",
+            );
+
+            let image = Image::from(ImageXObject {
+                width: Px(info.width as usize),
+                height: Px(info.height as usize),
+                color_space: match info.pixel_format {
+                    PixelFormat::L8 => ColorSpace::Greyscale,
+                    PixelFormat::RGB24 => ColorSpace::Rgb,
+                    PixelFormat::CMYK32 => ColorSpace::Cmyk,
+                },
+                bits_per_component: ColorBits::Bit8,
+                interpolate: false,
+                image_data: image,
+                image_filter: Some(ImageFilter::DCT),
+                clipping_bbox: None,
+            });
+
+            image.add_to_layer(
+                doc.get_page(page).get_layer(layer),
+                Some(Px(ori.translate_x() as usize).into_pt(dpi).into()),
+                Some(Px(ori.translate_y() as usize).into_pt(dpi).into()),
+                Some(ori.rotate_cw()),
+                Some(ori.scale_x()),
+                None,
+                Some(dpi),
+            );
+
+            Ok(())
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Error {
+    pub index: usize,
+    pub cause: Cause,
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self.cause {
+            Cause::PdfWrite(_) => self.cause.fmt(f),
+            _ => f.write_fmt(format_args!(
+                "error with JPEG index {}: {}",
+                self.index, self.cause
+            )),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
+#[derive(Debug)]
+pub enum Cause {
+    ImageInfo(jpeg_decoder::Error),
+    UnexpectedImageInfo,
+    ExifMetadata(exif::Error),
+    PdfWrite(printpdf::errors::Error),
+}
+
+impl Display for Cause {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        use Cause::*;
+        match self {
+            ImageInfo(e) => f.write_fmt(format_args!("failed to read image info: {}", e)),
+            UnexpectedImageInfo => {
+                f.write_fmt(format_args!("unexpectedly failed to read image info"))
+            }
+            ExifMetadata(e) => f.write_fmt(format_args!("failed to read EXIF metadata: {}", e)),
+            PdfWrite(e) => f.write_fmt(format_args!("failed to write PDF: {}", e)),
+        }
+    }
+}
+
+impl std::error::Error for Cause {}
+
+impl From<jpeg_decoder::Error> for Cause {
+    fn from(e: jpeg_decoder::Error) -> Cause {
+        Cause::ImageInfo(e)
+    }
+}
+
+impl From<exif::Error> for Cause {
+    fn from(e: exif::Error) -> Cause {
+        Cause::ExifMetadata(e)
+    }
+}
---
+++ src/ori.rs
@@ -0,0 +1,77 @@
+#[derive(Debug, Clone, Copy)]
+pub struct Orientation {
+    pub value: u16,
+    pub width: u16,
+    pub height: u16,
+}
+
+impl Orientation {
+    pub fn display_width(self) -> u16 {
+        if self.value > 4 {
+            self.height
+        } else {
+            self.width
+        }
+    }
+
+    pub fn display_height(self) -> u16 {
+        if self.value > 4 {
+            self.width
+        } else {
+            self.height
+        }
+    }
+
+    pub fn translate_x(self) -> u16 {
+        match self.value {
+            3..=4 => self.width,
+            7..=8 => self.height,
+            _ => 0,
+        }
+    }
+
+    pub fn translate_y(self) -> u16 {
+        match self.value {
+            3..=4 => self.height,
+            5..=6 => self.width,
+            _ => 0,
+        }
+    }
+
+    // based on testing, it seems like this is actually counterclockwise
+    // but printpdf still calls it `rotate_cw`
+    pub fn rotate_cw(self) -> f64 {
+        match self.value {
+            5..=6 => 270.0,
+            3..=4 => 180.0,
+            7..=8 => 90.0,
+            _ => 0.0,
+        }
+    }
+
+    pub fn scale_x(self) -> f64 {
+        match self.value {
+            2 | 4 | 5 | 7 => -1.0,
+            _ => 1.0,
+        }
+    }
+}
+
+// TODO add tests for the rest of the orientations
+
+#[test]
+fn orientation_6() {
+    let (width, height) = (4032, 3024);
+    let ori = Orientation {
+        value: 6,
+        width,
+        height,
+    };
+
+    assert_eq!(ori.display_width(), height);
+    assert_eq!(ori.display_height(), width);
+    assert_eq!(ori.translate_x(), 0);
+    assert_eq!(ori.translate_y(), width);
+    assert_eq!(ori.rotate_cw(), 270.0);
+    assert_eq!(ori.scale_x(), 1.0);
+}
